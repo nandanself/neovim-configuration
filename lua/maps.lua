8f929f2113fe91ddf49376fc14d777ffb2f8c299
nandanself <nandanself@gmail.com>
2022-12-31 22:13:59 +0530

    first commit: Basic configuration for nvim

---
+++ lua/maps.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = ' '
+local keymap = vim.keymap
+local silent = { silent = true }
+
+keymap.set('n', '<leader>q', '<cmd>q<cr>')
+keymap.set('n', '<leader>w', '<cmd>w<cr>')
+keymap.set('n', '<leader>x', '<cmd>x<cr>')
+
+keymap.set('n', '<c-a>', 'gg<S-v>G')
+
+keymap.set('n', '<leader>s', ':vsplit<Return><C-w>w', { silent = true })
+keymap.set('n', 'f', '<C-w>w')
+
+keymap.set('n', 'H', '^')
+keymap.set('n', 'L', '$')
+
+-- Better window movement
+keymap.set("n", "<C-h>", "<C-w>h", silent)
+keymap.set("n", "<C-j>", "<C-w>j", silent)
+keymap.set("n", "<C-k>", "<C-w>k", silent)
+keymap.set("n", "<C-l>", "<C-w>l", silent)
