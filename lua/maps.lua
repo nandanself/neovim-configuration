cb46842d797b370b733c2eea7c099cef0c8764ae
nandanself <nandanself@gmail.com>
2023-01-05 19:55:59 +0530

    added chatgpt

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,14 +1,15 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 local keymap = vim.keymap
 local silent = { silent = true }
 
-keymap.set('n', '<c-a>', 'gg<S-v>G')
+keymap.set("n", "<c-a>", "gg<S-v>G", silent)
+keymap.set("n", "<C-n>", ":ChatGPT<CR>", silent)
 
-keymap.set('n', '<leader>vs', ':vsplit<Return><C-w>w', silent)
-keymap.set('n', 'f', '<C-w>w')
+keymap.set("n", "<leader>vs", ":vsplit<Return><C-w>w", silent)
+keymap.set("n", "f", "<C-w>w")
 
-keymap.set('n', 'H', '^')
-keymap.set('n', 'L', '$')
+keymap.set("n", "H", "^")
+keymap.set("n", "L", "$")
 
 -- Better window movement
 keymap.set("n", "<C-h>", "<C-w>h", silent)
@@ -16,14 +17,14 @@ keymap.set("n", "<C-j>", "<C-w>j", silent)
 keymap.set("n", "<C-k>", "<C-w>k", silent)
 keymap.set("n", "<C-l>", "<C-w>l", silent)
 
-keymap.set("n", "<S-Tab>", ":BufferNext<CR>",silent)
+keymap.set("n", "<S-Tab>", ":BufferNext<CR>", silent)
 
-keymap.set("n", "<leader>1", ":BufferGoto 1<CR>",silent)
-keymap.set("n", "<leader>2", ":BufferGoto 2<CR>",silent)
-keymap.set("n", "<leader>3", ":BufferGoto 3<CR>",silent)
-keymap.set("n", "<leader>4", ":BufferGoto 4<CR>",silent)
-keymap.set("n", "<leader>5", ":BufferGoto 5<CR>",silent)
-keymap.set("n", "<leader>6", ":BufferGoto 6<CR>",silent)
-keymap.set("n", "<leader>7", ":BufferGoto 7<CR>",silent)
-keymap.set("n", "<leader>8", ":BufferGoto 8<CR>",silent)
-keymap.set("n", "<leader>9", ":BufferGoto 9<CR>",silent)
+keymap.set("n", "<leader>1", ":BufferGoto 1<CR>", silent)
+keymap.set("n", "<leader>2", ":BufferGoto 2<CR>", silent)
+keymap.set("n", "<leader>3", ":BufferGoto 3<CR>", silent)
+keymap.set("n", "<leader>4", ":BufferGoto 4<CR>", silent)
+keymap.set("n", "<leader>5", ":BufferGoto 5<CR>", silent)
+keymap.set("n", "<leader>6", ":BufferGoto 6<CR>", silent)
+keymap.set("n", "<leader>7", ":BufferGoto 7<CR>", silent)
+keymap.set("n", "<leader>8", ":BufferGoto 8<CR>", silent)
+keymap.set("n", "<leader>9", ":BufferGoto 9<CR>", silent)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,80 +1,96 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  }
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    tag = 'nightly'
-  }
-  use 'sainnhe/everforest'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	})
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		tag = "nightly",
+	})
+	use("sainnhe/everforest")
 
-  use 'rmagatti/alternate-toggler'
-  use 'windwp/nvim-autopairs'
-  use 'mg979/vim-visual-multi'
-  use 'gcmt/wildfire.vim'
-  use 'tpope/vim-surround'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-  use 'MattesGroeger/vim-bookmarks'
-  use 'tom-anders/telescope-vim-bookmarks.nvim'
+	use("rmagatti/alternate-toggler")
+	use("windwp/nvim-autopairs")
+	use("mg979/vim-visual-multi")
+	use("gcmt/wildfire.vim")
+	use("tpope/vim-surround")
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make", cond = vim.fn.executable("make") == 1 })
+	use("MattesGroeger/vim-bookmarks")
+	use("tom-anders/telescope-vim-bookmarks.nvim")
 
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'onsails/lspkind-nvim'
-  use { 'tami5/lspsaga.nvim', config = "require('lspsaga_config')" }
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("onsails/lspkind-nvim")
+	use({ "tami5/lspsaga.nvim", config = "require('lspsaga_config')" })
 
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'jose-elias-alvarez/typescript.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'windwp/nvim-ts-autotag'
-  use 'p00f/nvim-ts-rainbow'
-  use 'axelvc/template-string.nvim'
-  use 'j-hui/fidget.nvim'
-  use 'folke/todo-comments.nvim'
-  use({
-    "folke/noice.nvim",
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    }
-  })
-  use 'numToStr/Comment.nvim'
-  use 'AndrewRadev/sideways.vim'
-  use({
-    'akinsho/toggleterm.nvim',
-    tag = '*'
-  })
-  use 'tpope/vim-obsession'
-  use 'dhruvasagar/vim-prosession'
-  use('f-person/git-blame.nvim')
-  use { 'folke/which-key.nvim', config = "require('which_key_config')", event = "BufWinEnter" }
-  use 'lukas-reineke/indent-blankline.nvim'
-  use "folke/neodev.nvim"
-  use { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  }
-  use { "folke/twilight.nvim", config = "require('twilight_config')" }
-  use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
-use({"kylechui/nvim-surround",tag = "*",config = "require('surround_config')"})
-  use('github/copilot.vim')
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("jose-elias-alvarez/typescript.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("nvim-treesitter/nvim-treesitter")
+	use("windwp/nvim-ts-autotag")
+	use("p00f/nvim-ts-rainbow")
+	use("axelvc/template-string.nvim")
+	use("j-hui/fidget.nvim")
+	use("folke/todo-comments.nvim")
+	use({
+		"folke/noice.nvim",
+		requires = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+	})
+	use("numToStr/Comment.nvim")
+	use("AndrewRadev/sideways.vim")
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+	})
+	use("tpope/vim-obsession")
+	use("dhruvasagar/vim-prosession")
+	use("f-person/git-blame.nvim")
+	use({ "folke/which-key.nvim", config = "require('which_key_config')", event = "BufWinEnter" })
+	use("lukas-reineke/indent-blankline.nvim")
+	use("folke/neodev.nvim")
+	use({ -- Autocompletion
+		"hrsh7th/nvim-cmp",
+		requires = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
+	})
+	use({ "folke/twilight.nvim", config = "require('twilight_config')" })
+	use({ "romgrk/barbar.nvim", wants = "nvim-web-devicons" })
+	use({ "kylechui/nvim-surround", tag = "*", config = "require('surround_config')" })
+
+	use({
+		"jackMort/ChatGPT.nvim",
+		config = function()
+			require("chatgpt").setup({
+				-- optional configuration
+			})
+		end,
+		requires = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	})
+
+	use("github/copilot.vim")
 end)
