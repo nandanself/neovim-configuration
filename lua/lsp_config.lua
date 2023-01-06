8d2d38a14ac5e226400ab1af8c696822ff33dafd
maruti nandan <nandanself@gmail.com>
2023-01-06 17:14:48 +0530

    added LSP config

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -1,68 +1,77 @@
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
+	-- In this case, we create a function that lets us more easily define mappings specific
+	-- for LSP related items. It sets the mode, buffer and description for us each time.
 
-  -- local nmap = function(keys, func, desc)
-  --   if desc then
-  --     desc = 'LSP: ' .. desc
-  --   end
-  --
-  --   vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+	-- local nmap = function(keys, func, desc)
+	--   if desc then
+	--     desc = 'LSP: ' .. desc
+	--   end
+	--
+	--   vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
 
-  -- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  --
+	-- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+	--
 
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format()
+	end, { desc = "Format current buffer with LSP" })
 end
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lsp_config = {
-  capabilities = capabilities,
-  on_attach = function(_, bufnr)
-    on_attach(_, bufnr)
-  end
+	capabilities = capabilities,
+	on_attach = function(_, bufnr)
+		on_attach(_, bufnr)
+	end,
 }
 
-require('mason-lspconfig').setup_handlers({
-  function(server_name)
-    require('lspconfig')[server_name].setup(lsp_config)
-  end,
-  sumneko_lua = function()
-    require('lspconfig').sumneko_lua.setup(vim.tbl_extend('force', lsp_config, {
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { 'vim' }
-          }
-        }
-      }
-    }))
-  end,
-  tsserver = function()
-    require('typescript').setup({
-      server = vim.tbl_extend('force', lsp_config, {
-        on_attach = function(_, bufnr)
-          on_attach(_, bufnr)
-        end,
-        init_options = {
-          preferences = {
-            importModuleSpecifierPreference = 'project=relative',
-            jsxAttributeCompletionStylr = 'none'
-          }
-        }
-      })
-    })
-  end
+require("mason-lspconfig").setup_handlers({
+	function(server_name)
+		require("lspconfig")[server_name].setup(lsp_config)
+	end,
+	sumneko_lua = function()
+		require("lspconfig").sumneko_lua.setup(vim.tbl_extend("force", lsp_config, {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		}))
+	end,
+	tsserver = function()
+		require("typescript").setup({
+			server = vim.tbl_extend("force", lsp_config, {
+				on_attach = function(_, bufnr)
+					on_attach(_, bufnr)
+				end,
+				cmd = { "typescript-language-server", "--stdio" },
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+				},
+				init_options = {
+					preferences = {
+						importModuleSpecifierPreference = "project=relative",
+						jsxAttributeCompletionStylr = "none",
+					},
+				},
+			}),
+		})
+	end,
 })
 
-vim.keymap.set('n', '<leader>a', '<cmd>TypescriptAddMissingImports<cr>')
-vim.keymap.set('n', '<leader>r', '<cmd>TypescriptRemoveUnused<cr>')
+vim.keymap.set("n", "<leader>a", "<cmd>TypescriptAddMissingImports<cr>")
+vim.keymap.set("n", "<leader>r", "<cmd>TypescriptRemoveUnused<cr>")
--- lua/which_key_config.lua
+++ lua/which_key_config.lua
@@ -1,66 +1,65 @@
-local wk = require('which-key')
-wk.setup {
-  plugins = {
-    marks = false,
-    registers = false,
-    spelling = { enabled = false, suggestions = 20 },
-    presets = {
-      operators = false,
-      motions = false,
-      text_objects = false,
-      windows = false,
-      nav = false,
-      z = false,
-      g = false
-    }
-  }
-}
+local wk = require("which-key")
+wk.setup({
+	plugins = {
+		marks = false,
+		registers = false,
+		spelling = { enabled = false, suggestions = 20 },
+		presets = {
+			operators = false,
+			motions = false,
+			text_objects = false,
+			windows = false,
+			nav = false,
+			z = false,
+			g = false,
+		},
+	},
+})
 
 local mappings = {
-  q = { ':q<cr>', 'Quit' },
-  w = { ':w<cr>', 'Write' },
-  x = { ':x<cr>', 'Xxxx' },
+	q = { ":q<cr>", "Quit" },
+	w = { ":w<cr>", "Write" },
+	x = { ":x<cr>", "Xxxx" },
 
-  t = { ':Twilight<cr>', 'Twilight toggle' },
-  E = { ':e ~/.config/nvim/lua/plugins.lua<cr>', 'Edit nvim config' },
+	t = { ":Twilight<cr>", "Twilight toggle" },
+	E = { ":e ~/.config/nvim/lua/plugins.lua<cr>", "Edit nvim config" },
 
-  b = {
-    name = "Buffer",
+	b = {
+		name = "Buffer",
 
-    p = { "<Cmd>BufferPin<CR>", "Pin/Unpin Buffer" },
-    c = { "<Cmd>BufferCloseAllButVisible<CR>", "Close all Buffer" },
-  },
+		p = { "<Cmd>BufferPin<CR>", "Pin/Unpin Buffer" },
+		c = { "<Cmd>BufferCloseAllButVisible<CR>", "Close all Buffer" },
+	},
 
-  l = {
-    name = "LSP",
-    i = { ":LspInfo<cr>", "Connected Language Servers" },
-    h = { "<cmd>Lspsaga lsp_finder<CR>", "Lsp finder" },
-    k = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Signature Help" },
-    K = { "<cmd>Lspsaga hover_doc<cr>", "Hover Commands" },
-    w = { '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', "Add Workspace Folder" },
-    W = { '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', "Remove Workspace Folder" },
-    l = {
-      '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>',
-      "List Workspace Folders"
-    },
-    t = { '<cmd>lua vim.lsp.buf.type_definition()<cr>', "Type Definition" },
-    -- d = { '<cmd>lua vim.lsp.buf.definition()<cr>', "Go To Definition" },
+	l = {
+		name = "LSP",
+		i = { ":LspInfo<cr>", "Connected Language Servers" },
+		h = { "<cmd>Lspsaga lsp_finder<CR>", "Lsp finder" },
+		k = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Signature Help" },
+		K = { "<cmd>Lspsaga hover_doc<cr>", "Hover Commands" },
+		w = { "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>", "Add Workspace Folder" },
+		W = { "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>", "Remove Workspace Folder" },
+		l = {
+			"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>",
+			"List Workspace Folders",
+		},
+		t = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "Type Definition" },
+		d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Go To Definition" },
 
-    -- Peek Definition
-    -- you can edit the definition file in this flaotwindow
-    -- also support open/vsplit/etc operation check definition_action_keys
-    -- support tagstack C-t jump back
-    d = { '<cmd>Lspsaga preview_definition<cr>', "Go To Definition" },
+		-- Peek Definition
+		-- you can edit the definition file in this flaotwindow
+		-- also support open/vsplit/etc operation check definition_action_keys
+		-- support tagstack C-t jump back
+		-- p = { "<cmd>Lspsaga peek_definition<cr>", "Peek Definition" },
 
-    D = { '<cmd>lua vim.lsp.buf.declaration()<cr>', "Go To Declaration" },
-    r = { '<cmd>lua vim.lsp.buf.references()<cr>', "Goto References" },
-    R = { '<cmd>Lspsaga rename<cr>', "Rename" },
-    a = { '<cmd>Lspsaga code_action<cr>', "Code Action" },
-    e = { '<cmd>Lspsaga show_line_diagnostics<cr>', "Show Line Diagnostics" },
-    n = { '<cmd>Lspsaga diagnostic_jump_next<cr>', "Go To Next Diagnostic" },
-    N = { '<cmd>Lspsaga diagnostic_jump_prev<cr>', "Go To Previous Diagnostic" }
-  },
-
+		D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Go To Declaration" },
+		r = { "<cmd>lua vim.lsp.buf.references()<cr>", "Goto References" },
+		R = { "<cmd>Lspsaga rename<cr>", "Rename" },
+		a = { "<cmd>Lspsaga code_action<cr>", "Code Action" },
+		e = { "<cmd>Lspsaga show_line_diagnostics<cr>", "Show Line Diagnostics" },
+		n = { "<cmd>Lspsaga diagnostic_jump_next<cr>", "Go To Next Diagnostic" },
+		N = { "<cmd>Lspsaga diagnostic_jump_prev<cr>", "Go To Previous Diagnostic" },
+	},
 }
-local opts = { prefix = '<leader>' }
+local opts = { prefix = "<leader>" }
 wk.register(mappings, opts)
